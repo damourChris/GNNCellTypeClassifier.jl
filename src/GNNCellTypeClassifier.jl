ec94c4c49fe2ea153559d098470fbcf31f492078
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-09-21 01:22:22 +0200

    feat: add necessary imports and include files
    add necessary imports
    include files to the `GNNCellTypeClassifier.jl` module.
    ( `cell_gnn.jl`, `model.jl`, and `main.jl`)
    includes the `setup_env.jl` script

--- src/GNNCellTypeClassifier.jl
+++ src/GNNCellTypeClassifier.jl
@@ -1,5 +1,27 @@
 module GNNCellTypeClassifier
 
-# Write your package code here.
+include("setup_env.jl")
+
+using Flux
+using Flux: onecold, onehotbatch, logitcrossentropy, jacobian
+using GraphNeuralNetworks
+using LightXML
+using MLUtils
+using JLD2
+using Random
+using Plots
+using Statistics
+using TSne
+
+using ExpressionData
+using OntologyTrees
+using GNNCellTypeReferenceGraph
+
+include("./conversion.jl")
+include("./utils.jl")
+
+include("cell_gnn.jl")
+include("model.jl")
+include("main.jl")
 
 end
