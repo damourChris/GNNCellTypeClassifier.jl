f5ebe18a5f4013325a5b125190e164773e71e96f
Chris Damour <129872252+damourChris@users.noreply.github.com>
2024-09-22 18:43:05 +0000

    refactor: Update CellGNN struct and model function
    - Remove unused code and comments in `cell_gnn.jl`
    - Update `CellGNN` struct in `model.jl` to use `Chain` instead of `GraphNeuralNetworks`
    - Modify `CellGNN` function in `model.jl` to update the layers and computation flow
    - Add `eval_loss_accuracy` function to evaluate loss and accuracy in `model.jl`

--- src/model.jl
+++ src/model.jl
@@ -1,10 +1,30 @@
+struct CellGNN
+    chain::Chain
+end
 
-function create_model(nin, nh, nout; dropout=0.5)
-    return GNNChain(HeteroGraphConv((:gene, :to, :cell) => GraphConv(nin => 1, relu),
-                                    (:cell, :to, :cell) => GraphConv(nin => 1, relu)),
-                    GlobalPool(mean),
-                    Dropout(dropout),
-                    Dense(nh, nout))
+Flux.@layer :expand CellGNN
+
+function CellGNN(hidden_channels)
+    hidden = HeteroGraphConv((:gene, :to, :term) => GraphConv(1 => hidden_channels))
+    chain = Chain(;
+                  hidden=hidden,
+                  dense=Dense(hidden_channels => 1, relu))
+    return CellGNN(chain)
+end
+
+function (model::CellGNN)(g::GNNHeteroGraph)
+    gene_data_raw = g.ndata[:gene].exprs
+    term_data_raw = g.ndata[:term].proportion
+    gene_data = Float32.(reshape(gene_data_raw, 1, :))
+    term_data = Float32.(reshape(term_data_raw, 1, :))
+
+    l = model.chain.layers
+    x = (gene=gene_data, term=term_data)
+
+    x = l.hidden(g, x)
+    x = l.dense(x.term)
+
+    return x
 end
 
 function prepare_data(gnns::Vector{GNNHeteroGraph})
@@ -21,4 +41,22 @@ function prepare_data(gnns::Vector{GNNHeteroGraph})
     test_loader = DataLoader(test_data; shuffle=false, collate=true)
 
     return (train_loader, test_loader)
+end
+
+function eval_loss_accuracy(model, data_loader, device)
+    loss = 0.0
+    acc = 0.0
+    ntot = 0
+    for (g, y) in data_loader
+        # g, y = device(MLUtils.batch(g)), device(y)
+        n = length(y)
+
+        ŷ = model(g)
+
+        loss += Flux.crossentropy(ŷ, y) * n
+        acc += mean((ŷ .> 0) .== y) * n
+        ntot += n
+    end
+    return (loss=round(loss / ntot; digits=4),
+            acc=round(acc * 100 / ntot; digits=2))
 end
